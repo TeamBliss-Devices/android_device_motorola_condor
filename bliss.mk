12e00bc65b0b22151395d0404660506430020ca5
Patrick Graber <pjgraber03@gmail.com>
2015-07-24 17:29:22 -0700

    Blissified
    changed vendor path
    added in viper audio to build

--- bliss.mk
+++ bliss.mk
@@ -1,8 +1,11 @@
+#Bliss Audio Mod
+BLISS_AUDIO_MOD := viper
+
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := bliss_condor
 PRODUCT_RELEASE_NAME := MOTO E
